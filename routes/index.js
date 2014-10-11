3c4a3c57a8c9e0b7e19f03cedc871f39d5de0a96
Alan Lu <gotoalanlu@gmail.com>
2014-10-11 14:32:36 -0700

    Bla bla useful information

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+var pg = require('pg');
+
+router.get('/dbtest', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.error(err);
+            response.send(err);
+        } else {
+            client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send(err); }
+            else
+            { response.send(result.rows); }
+            });
+        }
+    });
+})
+
 module.exports = router;
